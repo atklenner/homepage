de781b9e9b74268d65e753f1c7c0e4896fb0c5d0
Andrew Klenner <atklenner@gmail.com>
2023-01-17 11:35:33 -0800

    center a bunch of divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Block from "./components/Block";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center h-screen bg-red-600">
+      <div className="flex justify-center items-center h-5/6 w-5/6 bg-red-200 rounded-2xl">
+        <div className="grid grid-cols-2 grid-rows-2 gap-16 h-5/6 w-5/6">
+          <Block type="time"/>
+          <Block type="weather"/>
+          <Block type="links"/>
+          <Block type="to-do list"/>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }

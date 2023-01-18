492e528afc6cf5d195696b93530fd411b37e06bc
Andrew Klenner <atklenner@gmail.com>
2023-01-18 11:04:09 -0800

    create generic block component for links and to-do items

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,11 +1,9 @@
-import Time from "./Time";
-
-function Widget({type}) {
+function Block({children}) {
   return (
-    <div className="bg-red-50 rounded-xl">
-      {type === "time" && <Time />}
+    <div className="bg-white py-2 px-4 m-2 rounded-lg w-5/6 flex justify-between">
+      {children}
     </div>
   )
 }
 
-export default Widget 
+export default Block;

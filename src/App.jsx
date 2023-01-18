7588798f4ac94c2ee0193160c0a393e7b9745f79
Andrew Klenner <atklenner@gmail.com>
2023-01-18 11:14:46 -0800

    widget container accepts child components making it more generic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Widget from "./components/Widget";
+import Time from "./components/Time";
+import ToDo from "./components/ToDo";
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-red-600">
       <div className="flex justify-center items-center h-5/6 w-5/6 bg-red-200 rounded-2xl">
         <div className="grid grid-cols-2 grid-rows-2 gap-16 h-5/6 w-5/6">
-          <Widget type="time"/>
-          <Widget type="weather"/>
-          <Widget type="to-do"/>
-          <Widget type="links"/>
+          <Widget>
+            <Time />
+          </Widget>
+          <Widget />
+          <Widget>
+            <ToDo />
+          </Widget>
+          <Widget />
         </div>
       </div>
     </div>
---
+++ src/components/Widget.jsx
@@ -0,0 +1,9 @@
+function Widget({children}) {
+  return (
+    <div className="bg-red-100 rounded-xl overflow-auto">
+      {children}
+    </div>
+  )
+}
+
+export default Widget 

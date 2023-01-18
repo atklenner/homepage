386788a547f2cd8bd4caebfc4ae0d78a94fe33fa
Andrew Klenner <atklenner@gmail.com>
2023-01-18 09:31:24 -0800

    change name of Block component to Widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Block from "./components/Block";
+import Widget from "./components/Block";
 
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-red-600">
       <div className="flex justify-center items-center h-5/6 w-5/6 bg-red-200 rounded-2xl">
         <div className="grid grid-cols-2 grid-rows-2 gap-16 h-5/6 w-5/6">
-          <Block type="time"/>
-          <Block type="weather"/>
-          <Block type="links"/>
-          <Block type="to-do list"/>
+          <Widget type="time"/>
+          <Widget type="weather"/>
+          <Widget type="links"/>
+          <Widget type="to-do list"/>
         </div>
       </div>
     </div>
--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,12 +1,11 @@
 import Time from "./Time";
 
-function Block({type}) {
+function Widget({type}) {
   return (
     <div className="bg-red-50 rounded-xl">
-      {type} block component
       {type === "time" && <Time />}
     </div>
   )
 }
 
-export default Block
+export default Widget 

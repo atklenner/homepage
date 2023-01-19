f7dbbceb8adae5da1d74687a118a816ba8eef28b
Andrew Klenner <atklenner@gmail.com>
2023-01-19 11:59:32 -0800

    implement reducer for handling state

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,19 +1,29 @@
 import Widget from "./Widget";
 import ToDoItem from "./ToDoItem";
+import ToDoInput from "./ToDoInput";
+import toDoReducer from "../reducers/toDoReducer";
+import {useReducer} from "react";
 
 function ToDo() {
-  let tasks = [
-    {id: 1, description: "a task", completed: false},
-    {id: 2, description: "another task", completed: true},
-  ]
-  const toDoItems = tasks.map(task => <ToDoItem key={task.id} {...task}/>);
+  const [tasks, dispatch] = useReducer(toDoReducer, initialTasks);
+  
+  const toDoItems = tasks.map(task => {
+    return <ToDoItem key={task.id} dispatch={dispatch} {...task}/>
+  });
+
   return (
     <Widget>
-    <div className="flex flex-col items-center p-2">
-      {toDoItems}
-    </div>
+      <div className="flex flex-col items-center p-2">
+        {toDoItems}
+        <ToDoInput dispatch={dispatch}/>
+      </div>
     </Widget>
   )
 }
 
 export default ToDo;
+
+const initialTasks = [
+    {id: 1, text: "a task", done: false},
+    {id: 2, text: "another task", done: true},
+  ];
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,12 +1,13 @@
 import Block from "./Block";
 
-function ToDoItem({id, description, completed}) {
-  let strikethrough = completed ? "text-gray-400 line-through" : "";
+function ToDoItem({id, text, done, dispatch}) {
+  let strikethrough = done ? "text-gray-400 line-through" : "";
+
   return (
     <Block>
-      <input type="checkbox" checked={completed}/>
-      <p className={strikethrough}>{description}</p>
-      <input type="button" value="edit"/>
+      <input type="checkbox" checked={done} onChange={() => dispatch({type:"toggle", id})}/>
+      <p className={strikethrough}>{text}</p>
+      <input type="button" value="del" onClick={() => dispatch({type:"delete", id})}/>
     </Block>
   )
 }

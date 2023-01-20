bc3bb11ffff0980f08bcd9ac1e6deb2cbe4ae8a5
Andrew Klenner <atklenner@gmail.com>
2023-01-20 11:10:49 -0800

    store tasks in localStorage

--- src/context/ToDoContext.jsx
+++ src/context/ToDoContext.jsx
@@ -1,4 +1,4 @@
-import {createContext, useReducer, useContext} from "react";
+import {createContext, useReducer, useContext, useEffect} from "react";
 import toDoReducer from "../reducers/toDoReducer";
 
 export const ToDoContext = createContext(null);
@@ -10,7 +10,23 @@ const initialTasks = [
   ];
 
 export function ToDoProvider({children}) {
-  const [tasks, dispatch] = useReducer(toDoReducer, initialTasks);
+  const [tasks, dispatch] = useReducer(toDoReducer, initialTasks, () => {
+    try {
+      const tasks = window.localStorage.getItem("tasks");
+      return tasks ? JSON.parse(tasks) : initialTasks;
+    } catch (error) {
+      console.log(error);
+      return initialTasks;
+    }
+  });
+  
+  useEffect(() => {
+    try {
+      window.localStorage.setItem("tasks", JSON.stringify(tasks));
+    } catch (error) {
+      console.log(error);
+    }
+  }, [tasks]);
 
   return (
     <ToDoContext.Provider value={tasks}>
@@ -19,7 +35,7 @@ export function ToDoProvider({children}) {
       </ToDoDispatchContext.Provider>
     </ToDoContext.Provider>
   )
-}
+} 
 
 export function useTasks() {
   return useContext(ToDoContext);

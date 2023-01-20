7ed4d21811030d151f278eeb15f121fd39550f94
Andrew Klenner <atklenner@gmail.com>
2023-01-19 18:00:13 -0800

    add to do context providers

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,25 +1,21 @@
 import Widget from "./Widget";
 import ToDoInput from "./ToDoInput";
 import ToDoList from "./ToDoList";
-import toDoReducer from "../reducers/toDoReducer";
-import {useReducer} from "react";
+import {ToDoProvider} from "../context/ToDoContext";
 
 function ToDo() {
-  const [tasks, dispatch] = useReducer(toDoReducer, initialTasks);
   
   return (
-    <Widget>
-      <div className="flex flex-col h-full w-full items-center p-2">
-        <ToDoList tasks={tasks} dispatch={dispatch}/>
-        <ToDoInput dispatch={dispatch}/>
-      </div>
-    </Widget>
+    <ToDoProvider>
+      <Widget>
+        <div className="flex flex-col h-full w-full items-center p-2">
+          <ToDoList/>
+          <ToDoInput/>
+        </div>
+      </Widget>
+    </ToDoProvider>
   )
 }
 
 export default ToDo;
 
-const initialTasks = [
-    {id: 1, text: "a task", done: false},
-    {id: 2, text: "another task", done: true},
-  ];

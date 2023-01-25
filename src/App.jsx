134fdfc057d12430a20bbf60a8cdb9999ce3b095
Andrew Klenner <atklenner@gmail.com>
2023-01-25 10:23:13 -0800

    changed Time to TimeAndWeather

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Time from "./components/Time";
+import TimeAndWeather from "./components/TimeAndWeather";
 import ToDo from "./components/ToDo";
 import Links from "./components/Links";
 
@@ -7,7 +7,7 @@ function App() {
     <div className="flex justify-center items-center h-screen bg-red-600">
       <div className="flex justify-center items-center h-5/6 w-5/6 bg-red-200 rounded-2xl">
         <div className="grid grid-cols-2 grid-rows-4 gap-16 h-5/6 w-5/6">
-          <Time />
+          <TimeAndWeather />
           <ToDo />
           <Links />
         </div>

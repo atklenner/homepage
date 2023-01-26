23d64bf7b47c28f5769bf6f2e0f8286b821e666e
Andrew Klenner <atklenner@gmail.com>
2023-01-26 11:45:58 -0800

    create placeholder components for settings so I stop getting annoying errors

---
+++ src/components/LinkSettings.jsx
@@ -0,0 +1,7 @@
+function LinkSettings() {
+  return (
+    <p>link settings</p>
+  )
+}
+
+export default LinkSettings;
---
+++ src/components/ThemeSettings.jsx
@@ -0,0 +1,7 @@
+function ThemeSettings() {
+  return (
+    <p>theme settings</p>
+  )
+}
+
+export default ThemeSettings;

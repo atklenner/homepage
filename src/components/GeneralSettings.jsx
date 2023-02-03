004f28a0f0e3117648e388fba0122bdb62d99436
Andrew Klenner <atklenner@gmail.com>
2023-02-03 11:10:17 -0800

    get the settings layout mostly ok

--- src/components/GeneralSettings.jsx
+++ src/components/GeneralSettings.jsx
@@ -4,51 +4,45 @@ import Widget from "./Widget";
 function GeneralSettings() {
   return (
     <Widget type="short">
-      <Block>
-        <fieldset>
-        <legend>Theme</legend>
-        <div>
-          <input id="latte" value="latte" name="theme" type="radio"/>
-          <label htmlFor="latte">Latte</label>
-        </div>
-        <div>
-          <input id="frappe" value="frappe" name="theme" type="radio"/>
-          <label htmlFor="frappe">Frappe</label>
-        </div>
-        <div>
-          <input id="macchiato" value="macchiato" name="theme" type="radio"/>
-          <label htmlFor="macchiato">Macchiato</label>
-        </div>
-          <input id="mocha" value="mocha" name="theme" type="radio" />
-          <label htmlFor="mocha">Mocha</label>
-        </fieldset>
-      </Block>
-      <Block>
-        <fieldset>
-          <legend>Temperature Units</legend>
-          <div>
-            <input id="celsius" value="celsius" name="units" type="radio"/>
-            <label htmlFor="celsius">Celsius</label>
+      <div className="flex flex-col gap-4">
+        <Block>
+          <fieldset>
+          <legend>Theme</legend>
+          <div> 
+            <input id="latte" value="latte" name="theme" type="radio"/>
+            <label className="mr-2" htmlFor="latte">Latte</label>
+            <input id="frappe" value="frappe" name="theme" type="radio"/>
+            <label className="mr-2" htmlFor="frappe">Frappe</label>
+            <input id="macchiato" value="macchiato" name="theme" type="radio"/>
+            <label className="mr-2" htmlFor="macchiato">Macchiato</label>
+            <input id="mocha" value="mocha" name="theme" type="radio" />
+            <label htmlFor="mocha">Mocha</label>
           </div>
-          <div>
-            <input id="fahrenheit" value="fahrenheit" name="units" type="radio"/>
-            <label htmlFor="fahrenheit">Fahrenheit</label>
-          </div>
-        </fieldset>
-      </Block>
-      <Block>
-        <fieldset>
-          <legend>Clock Settings</legend>
-          <div>
-            <input id="twelve" value="twelve" name="hours" type="radio"/>
-            <label htmlFor="twelve">Twelve Hour Clock</label>
-          </div>
-          <div>
-            <input id="twentyFour" value="twentyFour" name="hours" type="radio"/>
-            <label htmlFor="twentyFour">Twenty Four Hour Clock</label>
-          </div>
-        </fieldset>
-      </Block>
+          </fieldset>
+        </Block>
+        <Block>
+          <fieldset>
+            <legend>Temperature Units</legend>
+            <div>
+              <input id="celsius" value="celsius" name="units" type="radio"/>
+              <label className="mr-2" htmlFor="celsius">Celsius</label>
+              <input id="fahrenheit" value="fahrenheit" name="units" type="radio"/>
+              <label htmlFor="fahrenheit">Fahrenheit</label>
+            </div>
+          </fieldset>
+        </Block>
+        <Block>
+          <fieldset>
+            <legend>Clock Settings</legend>
+            <div>
+              <input id="twelve" value="twelve" name="hours" type="radio"/>
+              <label className="mr-2" htmlFor="twelve">Twelve Hour Clock</label>
+              <input id="twentyFour" value="twentyFour" name="hours" type="radio"/>
+              <label htmlFor="twentyFour">Twenty Four Hour Clock</label>
+            </div>
+          </fieldset>
+        </Block>
+      </div>
     </Widget>
   )
 }

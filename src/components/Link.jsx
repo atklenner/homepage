3b0e88aa4eb4326e595eb05c4afa74d81d63efea
Andrew Klenner <atklenner@gmail.com>
2023-03-15 12:13:58 -0700

    change link text size on smaller screens

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,11 +3,17 @@ import Block from "./Block";
 function Link({ url, text }) {
   return (
     <Block>
-      {url && <a href={url} className="h-full w-full flex justify-center items-center text-lg font-medium" target="_blank">
-        {text}
-      </a>}
+      {url && (
+        <a
+          href={url}
+          className="h-full w-full flex justify-center items-center md:text-lg font-medium"
+          target="_blank"
+        >
+          {text}
+        </a>
+      )}
     </Block>
-  )
+  );
 }
 
 export default Link;
